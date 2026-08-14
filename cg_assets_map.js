8b531a7ada68446a27afd014f2ffacf9d7ecf28c
mosquito <1915046496zzy@gmail.com>
2026-08-14 21:51:38 +0800

    Create cg_assets_map.js

---
+++ cg_assets_map.js
@@ -0,0 +1,81 @@
+// 全局 CG 图床资源映射表
+var CG_AssetsMap = {
+
+    // ==========================================
+    //                 林岁岁
+    // ==========================================
+
+    // 【SFW 日常】------------------------------
+    // (暂空，预留位置)
+
+    // 【NSFW 亲密】-----------------------------
+    // --- 抱入 ---
+    "CG_linsuisui_baoru_1": "https://i.postimg.cc/Fs0D2TNp/linsuisui-baoru-1.jpg",
+    "CG_linsuisui_baoru_2": "https://i.postimg.cc/m2780mBS/linsuisui-baoru-2.jpg",
+    "CG_linsuisui_baoru_3": "https://i.postimg.cc/VLXD3Km4/linsuisui-baoru-3.jpg",
+    "CG_linsuisui_baoru_4": "https://i.postimg.cc/fTYB68DK/linsuisui-baoru-4.jpg",
+    "CG_linsuisui_baoru_5": "https://i.postimg.cc/MK1dh9q3/linsuisui-baoru-5.jpg",
+    "CG_linsuisui_baoru_6": "https://i.postimg.cc/kXWfkTqH/linsuisui-baoru-6.jpg",
+    "CG_linsuisui_baoru_7": "https://i.postimg.cc/MK1dh9qg/linsuisui-baoru-7.jpg",
+
+    // --- 侧入 ---
+    "CG_linsuisui_ceru_1": "https://i.postimg.cc/3xKCz7L4/linsuisui-ceru-1.jpg",
+    "CG_linsuisui_ceru_2": "https://i.postimg.cc/Njg7ZYNT/linsuisui-ceru-2.jpg",
+    "CG_linsuisui_ceru_3": "https://i.postimg.cc/FH9gwmPL/linsuisui-ceru-3.jpg",
+
+    // --- 传教士 ---
+    "CG_linsuisui_chuanjiaoshi_1": "https://i.postimg.cc/cJzH7Mg8/linsuisui-chuanjiaoshi-1.jpg",
+    "CG_linsuisui_chuanjiaoshi_2": "https://i.postimg.cc/7LR61MGJ/linsuisui-chuanjiaoshi-2.jpg",
+    "CG_linsuisui_chuanjiaoshi_3": "https://i.postimg.cc/W3J45Fgb/linsuisui-chuanjiaoshi-3.jpg",
+    "CG_linsuisui_chuanjiaoshi_4": "https://i.postimg.cc/hGYj8bzm/linsuisui-chuanjiaoshi-4.jpg",
+    "CG_linsuisui_chuanjiaoshi_5": "https://i.postimg.cc/QtWMf91c/linsuisui-chuanjiaoshi-5.jpg",
+    "CG_linsuisui_chuanjiaoshi_6": "https://i.postimg.cc/hj7G2zTr/linsuisui-chuanjiaoshi-6.jpg",
+    "CG_linsuisui_chuanjiaoshi_7": "https://i.postimg.cc/zBRfxbKx/linsuisui-chuanjiaoshi-7.jpg",
+
+    // --- 肛交 ---
+    "CG_linsuisui_gangjiao_1": "https://i.postimg.cc/J4KRccS1/linsuisui-gangjiao-1.jpg",
+    "CG_linsuisui_gangjiao_2": "https://i.postimg.cc/rFgqCC7M/linsuisui-gangjiao-2.jpg",
+
+    // --- 后入 ---
+    "CG_linsuisui_houru_1": "https://i.postimg.cc/9FxjqXwv/linsuisui-houru-1.jpg",
+    "CG_linsuisui_houru_2": "https://i.postimg.cc/pX0tnVnN/linsuisui-houru-2.jpg",
+    "CG_linsuisui_houru_3": "https://i.postimg.cc/8PyVrkf2/linsuisui-houru-3.jpg",
+    "CG_linsuisui_houru_4": "https://i.postimg.cc/NfNwrGrt/linsuisui-houru-4.jpg",
+    "CG_linsuisui_houru_5": "https://i.postimg.cc/4N8Th4hd/linsuisui-houru-5.jpg",
+    "CG_linsuisui_houru_6": "https://i.postimg.cc/7YByTPTY/linsuisui-houru-6.jpg",
+    "CG_linsuisui_houru_7": "https://i.postimg.cc/Kv9ygcg8/linsuisui-houru-7.jpg",
+    "CG_linsuisui_houru_8": "https://i.postimg.cc/zGVYwmnJ/linsuisui-houru-8.jpg",
+    "CG_linsuisui_houru_9": "https://i.postimg.cc/VkdP9yjd/linsuisui-houru-9.jpg",
+    "CG_linsuisui_houru_10": "https://i.postimg.cc/Z5CzPkry/linsuisui-houru-10.jpg",
+
+    // --- 裸体 ---
+    "CG_linsuisui_luoti_1": "https://i.postimg.cc/gktf2MfL/linsuisui-luoti-1.jpg",
+    "CG_linsuisui_luoti_2": "https://i.postimg.cc/RVpjZPjn/linsuisui-luoti-2.jpg",
+
+    // --- 女上位 ---
+    "CG_linsuisui_nvshangwei_1": "https://i.postimg.cc/90RytQFN/linsuisui-nvshangwei-1.jpg",
+    "CG_linsuisui_nvshangwei_2": "https://i.postimg.cc/Gt8vJmh5/linsuisui-nvshangwei-2.jpg",
+    "CG_linsuisui_nvshangwei_3": "https://i.postimg.cc/Fzd0gHs2/linsuisui-nvshangwei-3.jpg",
+    "CG_linsuisui_nvshangwei_4": "https://i.postimg.cc/T1Lnq338/linsuisui-nvshangwei-4.jpg",
+    "CG_linsuisui_nvshangwei_5": "https://i.postimg.cc/wMCLWY1v/linsuisui-nvshangwei-5.jpg",
+    "CG_linsuisui_nvshangwei_6": "https://i.postimg.cc/T1zgkvKh/linsuisui-nvshangwei-6.jpg",
+    "CG_linsuisui_nvshangwei_7": "https://i.postimg.cc/1XkwW18X/linsuisui-nvshangwei-7.jpg",
+    "CG_linsuisui_nvshangwei_8": "https://i.postimg.cc/C53kvpRf/linsuisui-nvshangwei-8.jpg",
+    "CG_linsuisui_nvshangwei_9": "https://i.postimg.cc/zvsKdYyT/linsuisui-nvshangwei-9.jpg",
+    "CG_linsuisui_nvshangwei_10": "https://i.postimg.cc/J0fZTCsb/linsuisui-nvshangwei-10.jpg",
+    "CG_linsuisui_nvshangwei_11": "https://i.postimg.cc/2jMQjh2H/linsuisui-nvshangwei-11.jpg",
+    "CG_linsuisui_nvshangwei_12": "https://i.postimg.cc/jqmHqPZB/linsuisui-nvshangwei-12.jpg",
+    "CG_linsuisui_nvshangwei_13": "https://i.postimg.cc/nVg4VB3t/linsuisui-nvshangwei-13.jpg",
+
+
+    // ==========================================
+    //                 望秋雁
+    // ==========================================
+
+    // 【SFW 日常】------------------------------
+    // (暂空，预留位置)
+
+    // 【NSFW 亲密】-----------------------------
+    // (暂空，预留位置)
+
+};
